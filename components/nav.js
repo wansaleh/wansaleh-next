584b382e008ad0e55e7c5a5ddd3f1cfc4b48a0f0
Wan Saleh <wansaleh@gmail.com>
2019-06-27 13:03:11 +0800

    Update.

--- components/nav.js
+++ components/nav.js
@@ -49,7 +49,7 @@ const Nav = () => (
           padding: 6px 8px;
         }
         a {
-          color: #067df7;
+          color: #000;
           text-decoration: none;
           font-size: 13px;
         }
