24f2f122b458d59da59e663588ab683056f500a4
Wan Saleh <wansaleh@gmail.com>
2020-08-09 02:34:40 +0800

    Updates.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import { ChakraProvider, CSSReset } from '@chakra-ui/core';
 
+import { useEffect } from 'react';
+import SmoothScroll from '../lib/smoothscroll';
+
 import chakra from '../styles/chakra';
 import '../styles/main.scss';
 import '../styles/font-graphik.css';
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    SmoothScroll(document, 50, 8);
+  }, []);
+
   return (
     <ChakraProvider theme={chakra}>
       <CSSReset />
