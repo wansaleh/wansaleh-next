0cce741baaac3d7441b6a1ae5d7263cc87ba0c3a
Wan Saleh <wansaleh@gmail.com>
2021-04-30 22:11:25 +0800

    Updates.

--- src/components/discography.jsx
+++ src/components/discography.jsx
@@ -1,13 +1,15 @@
 import {
   AspectRatio,
   Box,
+  Button,
   // chakra,
   Flex,
   Heading,
   HStack,
   LinkBox,
   LinkOverlay,
-  SimpleGrid
+  SimpleGrid,
+  VisuallyHidden
 } from '@chakra-ui/react';
 import arrayToSentence from 'array-to-sentence';
 import { usePalette } from 'color-thief-react';
@@ -138,7 +140,7 @@ function Work({ work }) {
   const cellHeight = coverHeight + 100;
 
   return (
-    <LazyLoad height={cellHeight} unmountIfInvisible classNamePrefix="ll">
+    <LazyLoad height={cellHeight} classNamePrefix="ll">
       <LinkBox key={work.youtube} role="group" h={`${cellHeight}px`}>
         <LinkOverlay
           href={`https://youtube.com/watch?v=${work.youtube}`}
@@ -185,6 +187,7 @@ function Work({ work }) {
                     pointerEvents: 'none'
                   }}
                 />
+
                 <HStack
                   className="front"
                   justify="center"
@@ -319,6 +322,50 @@ function Work({ work }) {
             </Flex>
           </Flex>
         </LinkOverlay>
+
+        {work.spotify && (
+          <Box
+            pos="absolute"
+            left="0"
+            top="0"
+            color={palette ? readableColor(palette[PALETTENUM]) : 'white'}
+            opacity="0"
+            transition="all 0.3s ease"
+            _groupHover={{
+              opacity: 1
+            }}
+          >
+            <Button
+              variant="unstyled"
+              p="2"
+              m="3"
+              h="auto"
+              minW="unset"
+              minH="unset"
+              d="block"
+              cursor="pointer"
+              // color="white"
+              onClick={() => window.open(work.spotify)}
+            >
+              <Box
+                as="svg"
+                d="block"
+                w="1.5rem"
+                h="1.5rem"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+              >
+                <g fillRule="evenodd" stroke="none" strokeWidth="1">
+                  <g transform="translate(-200.000000, -460.000000)">
+                    <path d="M238.16,481.36 C230.48,476.8 217.64,476.32 210.32,478.6 C209.12,478.96 207.92,478.24 207.56,477.16 C207.2,475.96 207.92,474.76 209,474.4 C217.52,471.88 231.56,472.36 240.44,477.64 C241.52,478.24 241.88,479.68 241.28,480.76 C240.68,481.6 239.24,481.96 238.16,481.36 M237.92,488.08 C237.32,488.92 236.24,489.28 235.4,488.68 C228.92,484.72 219.08,483.52 211.52,485.92 C210.56,486.16 209.48,485.68 209.24,484.72 C209,483.76 209.48,482.68 210.44,482.44 C219.2,479.8 230,481.12 237.44,485.68 C238.16,486.04 238.52,487.24 237.92,488.08 M235.04,494.68 C234.56,495.4 233.72,495.64 233,495.16 C227.36,491.68 220.28,490.96 211.88,492.88 C211.04,493.12 210.32,492.52 210.08,491.8 C209.84,490.96 210.44,490.24 211.16,490 C220.28,487.96 228.2,488.8 234.44,492.64 C235.28,493 235.4,493.96 235.04,494.68 M224,460 C210.8,460 200,470.8 200,484 C200,497.2 210.8,508 224,508 C237.2,508 248,497.2 248,484 C248,470.8 237.32,460 224,460" />
+                  </g>
+                </g>
+              </Box>
+              <VisuallyHidden>Spotify</VisuallyHidden>
+            </Button>
+          </Box>
+        )}
       </LinkBox>
     </LazyLoad>
   );
