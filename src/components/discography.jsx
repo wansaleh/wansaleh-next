af697fb11c421d7a3560ddde0fd07d33bd2b368e
Wan Saleh <wansaleh@gmail.com>
2021-05-04 18:11:11 +0800

    Updates.

--- src/components/discography.jsx
+++ src/components/discography.jsx
@@ -155,32 +155,28 @@ function Work({ work }) {
   const cellHeight = coverHeight + 120;
 
   return (
-    <LinkBox key={work.youtube} role="group" h={`${cellHeight}px`}>
-      <LinkOverlay
-        href={`https://youtube.com/watch?v=${work.youtube}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Flex
-          direction="column"
-          bg={
-            palette
-              ? `linear-gradient(to bottom right, ${adjustHue(
-                  -5,
-                  darken(0.1, palette[PALETTENUM])
-                )}, ${adjustHue(10, lighten(0.2, palette[PALETTENUM]))})`
-              : 'gray.800'
-          }
-          color={palette ? readableColor(palette[PALETTENUM]) : 'white'}
-          transition="all 0.2s ease"
-          textAlign="center"
-          p="4"
-          h="full"
+    <LazyLoad key={work.youtube} height={cellHeight} classNamePrefix="ll">
+      <LinkBox key={work.youtube} role="group" h={`${cellHeight}px`}>
+        <LinkOverlay
+          href={`https://youtube.com/watch?v=${work.youtube}`}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <LazyLoad
-            height={coverHeight}
-            classNamePrefix="ll"
-            placeholder={<Placeholder height={coverHeight} />}
+          <Flex
+            direction="column"
+            bg={
+              palette
+                ? `linear-gradient(to bottom right, ${adjustHue(
+                    -5,
+                    darken(0.1, palette[PALETTENUM])
+                  )}, ${adjustHue(10, lighten(0.2, palette[PALETTENUM]))})`
+                : 'gray.800'
+            }
+            color={palette ? readableColor(palette[PALETTENUM]) : 'white'}
+            transition="all 0.2s ease"
+            textAlign="center"
+            p="4"
+            h="full"
           >
             <AspectRatio
               ref={ref}
@@ -260,155 +256,161 @@ function Work({ work }) {
                 </HStack>
               </Box>
             </AspectRatio>
-          </LazyLoad>
 
-          <Flex
-            w="full"
-            flex="1"
-            direction="column"
-            justify="center"
-            align="center"
-            px="4"
-            // py="2"
-            mb="-4"
-            lineHeight="1"
-          >
-            <Heading
-              as="h3"
+            <Flex
               w="full"
-              fontSize="lg"
-              fontWeight="600"
-              d="flex"
-              justifyContent="center"
-              alignItems="center"
+              flex="1"
+              direction="column"
+              justify="center"
+              align="center"
+              px="4"
+              // py="2"
+              mb="-4"
+              lineHeight="1"
             >
-              <Box
-                as="span"
-                d="inline-block"
-                maxW="90%"
-                whiteSpace="nowrap"
-                overflow="hidden"
-                textOverflow="ellipsis"
+              <Heading
+                as="h3"
+                w="full"
+                fontSize="lg"
+                fontWeight="600"
+                d="flex"
+                justifyContent="center"
+                alignItems="center"
               >
-                {work.song}
-              </Box>
-
-              {isAfter(work.released, subWeeks(new Date(), 8)) && (
-                <SmallBadge
-                  color={palette && palette[PALETTENUM]}
-                  ml="1.5"
-                  // mt="0.5"
+                <Box
+                  as="span"
+                  d="inline-block"
+                  maxW="90%"
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  textOverflow="ellipsis"
                 >
-                  New
-                </SmallBadge>
-              )}
-            </Heading>
+                  {work.song}
+                </Box>
 
-            <Flex justify="center" align="center" direction="column" w="full">
-              <Box
-                fontSize="sm"
-                fontWeight="500"
-                maxW="90%"
-                whiteSpace="nowrap"
-                overflow="hidden"
-                textOverflow="ellipsis"
-                mb="2"
-              >
-                {listNames(work.artist)}
-              </Box>
+                {isAfter(work.released, subWeeks(new Date(), 8)) && (
+                  <SmallBadge
+                    color={palette && palette[PALETTENUM]}
+                    ml="1.5"
+                    // mt="0.5"
+                  >
+                    New
+                  </SmallBadge>
+                )}
+              </Heading>
+
+              <Flex justify="center" align="center" direction="column" w="full">
+                <Box
+                  fontSize="sm"
+                  fontWeight="500"
+                  maxW="90%"
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                  mb="2"
+                >
+                  {listNames(work.artist)}
+                </Box>
+
+                <Box
+                  mt="1"
+                  fontSize="xs"
+                  fontWeight="600"
+                  maxW="90%"
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                  opacity="0"
+                  transition="all 0.3s ease"
+                  transform="translateY(50%)"
+                  _groupHover={{ opacity: 1, transform: 'translateY(0)' }}
+                >
+                  {work.composer ? (
+                    <>Written by {listNames(work.composer)}</>
+                  ) : (
+                    <>&nbsp;</>
+                  )}
+                </Box>
+              </Flex>
 
               <Box
                 mt="1"
                 fontSize="xs"
-                fontWeight="600"
-                maxW="90%"
-                whiteSpace="nowrap"
-                overflow="hidden"
-                textOverflow="ellipsis"
-                opacity="0"
-                transition="all 0.3s ease"
-                transform="translateY(50%)"
-                _groupHover={{ opacity: 1, transform: 'translateY(0)' }}
+                fontWeight="400"
+                pos="relative"
+                w="full"
               >
-                {work.composer ? (
-                  <>Written by {listNames(work.composer)}</>
-                ) : (
-                  <>&nbsp;</>
-                )}
+                <Box
+                  transition="all 0.3s ease"
+                  _groupHover={{ opacity: 0 }}
+                  textTransform="capitalize"
+                >
+                  {formatDistanceToNow(work.released, { addSuffix: true })}
+                </Box>
+                <Box
+                  opacity="0"
+                  pos="absolute"
+                  inset="0"
+                  transition="all 0.3s ease"
+                  _groupHover={{ opacity: 1 }}
+                  textTransform="capitalize"
+                >
+                  {format(work.released, 'd MMMM yyy')}
+                </Box>
               </Box>
             </Flex>
-
-            <Box mt="1" fontSize="xs" fontWeight="400" pos="relative" w="full">
-              <Box
-                transition="all 0.3s ease"
-                _groupHover={{ opacity: 0 }}
-                textTransform="capitalize"
-              >
-                {formatDistanceToNow(work.released, { addSuffix: true })}
-              </Box>
-              <Box
-                opacity="0"
-                pos="absolute"
-                inset="0"
-                transition="all 0.3s ease"
-                _groupHover={{ opacity: 1 }}
-                textTransform="capitalize"
-              >
-                {format(work.released, 'd MMMM yyy')}
-              </Box>
-            </Box>
           </Flex>
-        </Flex>
-      </LinkOverlay>
+        </LinkOverlay>
 
-      {work.spotify && (
-        <Box
-          pos="absolute"
-          left="3"
-          top="3"
-          color="white"
-          // color={palette ? readableColor(palette[PALETTENUM]) : 'white'}
-          opacity="0"
-          transform="scale(0.9)"
-          transformOrigin="top left"
-          transition="all 0.3s ease"
-          _groupHover={{
-            opacity: 1,
-            transform: 'scale(1)'
-          }}
-        >
-          <Button
-            variant="unstyled"
-            p="2"
-            m="1"
-            h="auto"
-            minW="unset"
-            minH="unset"
-            d="block"
-            cursor="pointer"
-            // color="white"
-            onClick={() => window.open(work.spotify)}
+        {work.spotify && (
+          <Box
+            pos="absolute"
+            left="3"
+            top="3"
+            color="white"
+            // color={palette ? readableColor(palette[PALETTENUM]) : 'white'}
+            opacity="0"
+            transform="scale(0.9)"
+            transformOrigin="top left"
+            transition="all 0.3s ease"
+            _groupHover={{
+              opacity: 1,
+              transform: 'scale(1)'
+            }}
           >
-            <Box
-              as="svg"
+            <Button
+              variant="unstyled"
+              p="2"
+              m="1"
+              h="auto"
+              minW="unset"
+              minH="unset"
               d="block"
-              w="1.5rem"
-              h="1.5rem"
-              viewBox="0 0 48 48"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
+              cursor="pointer"
+              // color="white"
+              onClick={() => window.open(work.spotify)}
             >
-              <g fillRule="evenodd" stroke="none" strokeWidth="1">
-                <g transform="translate(-200.000000, -460.000000)">
-                  <path d="M238.16,481.36 C230.48,476.8 217.64,476.32 210.32,478.6 C209.12,478.96 207.92,478.24 207.56,477.16 C207.2,475.96 207.92,474.76 209,474.4 C217.52,471.88 231.56,472.36 240.44,477.64 C241.52,478.24 241.88,479.68 241.28,480.76 C240.68,481.6 239.24,481.96 238.16,481.36 M237.92,488.08 C237.32,488.92 236.24,489.28 235.4,488.68 C228.92,484.72 219.08,483.52 211.52,485.92 C210.56,486.16 209.48,485.68 209.24,484.72 C209,483.76 209.48,482.68 210.44,482.44 C219.2,479.8 230,481.12 237.44,485.68 C238.16,486.04 238.52,487.24 237.92,488.08 M235.04,494.68 C234.56,495.4 233.72,495.64 233,495.16 C227.36,491.68 220.28,490.96 211.88,492.88 C211.04,493.12 210.32,492.52 210.08,491.8 C209.84,490.96 210.44,490.24 211.16,490 C220.28,487.96 228.2,488.8 234.44,492.64 C235.28,493 235.4,493.96 235.04,494.68 M224,460 C210.8,460 200,470.8 200,484 C200,497.2 210.8,508 224,508 C237.2,508 248,497.2 248,484 C248,470.8 237.32,460 224,460" />
+              <Box
+                as="svg"
+                d="block"
+                w="1.5rem"
+                h="1.5rem"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+              >
+                <g fillRule="evenodd" stroke="none" strokeWidth="1">
+                  <g transform="translate(-200.000000, -460.000000)">
+                    <path d="M238.16,481.36 C230.48,476.8 217.64,476.32 210.32,478.6 C209.12,478.96 207.92,478.24 207.56,477.16 C207.2,475.96 207.92,474.76 209,474.4 C217.52,471.88 231.56,472.36 240.44,477.64 C241.52,478.24 241.88,479.68 241.28,480.76 C240.68,481.6 239.24,481.96 238.16,481.36 M237.92,488.08 C237.32,488.92 236.24,489.28 235.4,488.68 C228.92,484.72 219.08,483.52 211.52,485.92 C210.56,486.16 209.48,485.68 209.24,484.72 C209,483.76 209.48,482.68 210.44,482.44 C219.2,479.8 230,481.12 237.44,485.68 C238.16,486.04 238.52,487.24 237.92,488.08 M235.04,494.68 C234.56,495.4 233.72,495.64 233,495.16 C227.36,491.68 220.28,490.96 211.88,492.88 C211.04,493.12 210.32,492.52 210.08,491.8 C209.84,490.96 210.44,490.24 211.16,490 C220.28,487.96 228.2,488.8 234.44,492.64 C235.28,493 235.4,493.96 235.04,494.68 M224,460 C210.8,460 200,470.8 200,484 C200,497.2 210.8,508 224,508 C237.2,508 248,497.2 248,484 C248,470.8 237.32,460 224,460" />
+                  </g>
                 </g>
-              </g>
-            </Box>
-            <VisuallyHidden>Spotify</VisuallyHidden>
-          </Button>
-        </Box>
-      )}
-    </LinkBox>
+              </Box>
+              <VisuallyHidden>Spotify</VisuallyHidden>
+            </Button>
+          </Box>
+        )}
+      </LinkBox>
+    </LazyLoad>
   );
 }
 
