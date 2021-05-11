8c2eb694ff1f0c41b335089407a5a9ab7d89b009
Wan Saleh <wansaleh@gmail.com>
2021-05-12 06:27:47 +0800

    Updates.

--- src/components/page-header.jsx
+++ src/components/page-header.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Heading, useColorModeValue } from '@chakra-ui/react';
+import { Box, Container, Heading } from '@chakra-ui/react';
 
 export default function PageHeader({ title, middle, subtitle }) {
   return (
