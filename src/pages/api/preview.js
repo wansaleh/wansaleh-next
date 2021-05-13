d6826467fd25b91ad0e48d52ca3417c796d6219d
Wan Saleh <wansaleh@gmail.com>
2021-05-14 04:26:26 +0800

    Updates.

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -21,6 +21,6 @@ export default async function handler(req, res) {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/posts/${post.slug}` });
+  res.writeHead(307, { Location: `/blog/${post.slug}` });
   res.end();
 }
