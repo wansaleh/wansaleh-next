36fb0acf1a4d40a858d034b01ed4aae78bd176a2
Wan Saleh <wansaleh@gmail.com>
2021-05-13 10:44:59 +0800

    Updates.

--- next.config.js
+++ next.config.js
@@ -2,34 +2,21 @@ const withPlugins = require('next-compose-plugins');
 // const withFonts = require('next-fonts');
 const withImages = require('next-optimized-images');
 
-module.exports = withPlugins(
-  [
-    [
-      withImages
-      // {
-      //   svgo: {
-      //     plugins: [{ cleanupIDs: false }]
-      //   }
-      // }
-    ]
-    // // [withFonts]
-  ],
-  {
-    future: {
-      webpack5: true
-    },
-    images: {
-      domains: [
-        'res.cloudinary.com',
-        'media.graphcms.com',
-        'is1-ssl.mzstatic.com',
-        'is2-ssl.mzstatic.com',
-        'is3-ssl.mzstatic.com',
-        'is4-ssl.mzstatic.com',
-        'is5-ssl.mzstatic.com',
-        'i.scdn.co',
-        'i.ytimg.com'
-      ]
-    }
+module.exports = withPlugins([[withImages]], {
+  future: {
+    webpack5: true
   }
-);
+  // images: {
+  //   domains: [
+  //     'res.cloudinary.com',
+  //     'media.graphcms.com',
+  //     'is1-ssl.mzstatic.com',
+  //     'is2-ssl.mzstatic.com',
+  //     'is3-ssl.mzstatic.com',
+  //     'is4-ssl.mzstatic.com',
+  //     'is5-ssl.mzstatic.com',
+  //     'i.scdn.co',
+  //     'i.ytimg.com'
+  //   ]
+  // }
+});
