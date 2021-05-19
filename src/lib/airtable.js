7e0f71eb432c9e461e6b2dcc43168595dbbb3fab
Wan Saleh <wansaleh@gmail.com>
2021-05-19 15:02:23 +0800

    Updates.

--- src/lib/airtable.js
+++ src/lib/airtable.js
@@ -3,7 +3,7 @@ import qs from 'qs';
 export async function fetchWorks({ works = [], limit = 100, offset = null } = {}) {
   const data = await fetch(
     `https://api.airtable.com/v0/app4IuhsxXqAX7tha/Works?${qs.stringify({
-      view: 'ShowOnSite',
+      view: 'Show On Website',
       pageSize: limit,
       offset
     })}`,
