6868590e3f48b4dc1bfa9d80478e212f7763cf4a
Wan Saleh <wansaleh@gmail.com>
2021-10-27 04:01:21 +0800

    Updates.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-const withPlugins = require('next-compose-plugins');
-// const withImages = require('next-optimized-images');
-
-module.exports = withPlugins([], {
-  experimental: { esmExternals: true }
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //     'media.graphcms.com',
-  //     'is1-ssl.mzstatic.com',
-  //     'is2-ssl.mzstatic.com',
-  //     'is3-ssl.mzstatic.com',
-  //     'is4-ssl.mzstatic.com',
-  //     'is5-ssl.mzstatic.com',
-  //     'i.scdn.co',
-  //     'i.ytimg.com'
-  //   ]
-  // }
-});
+module.exports = {
+  experimental: { esmExternals: false },
+};
